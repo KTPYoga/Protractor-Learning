8c6e260a7a7c75be027b04ea88195e6a17719d6c
abot <pooja.chatterjee@kaplan.com>
2016-12-20 08:42:42 -0500

    dashboard tests

--- e2e/qbank/channelPage.po.ts
+++ e2e/qbank/channelPage.po.ts
@@ -1,9 +1,30 @@
 import { browser, element, by, protractor} from 'protractor';
+import { envConfig } from './envConfig';
 
+var config = 'feature6';
 var kec_username = 'pchatterjee';
-var kec_password = 'Welcome1';
+var kec_password = 'Welcome2';
+let env = new envConfig();
+
 
 export class channelPage{
+
+    //elements
+    ICON = 'db-assignment-progress div.assignment__group__label span';
+    AssignmenType = 'db-assignment-progress div.assignment__group__label h4';
+    ProgressComplete = 'db-assignment-progress div.assignment__group__summary h5';
+    TimeRemaining = this.ProgressComplete + ' span';
+
+
+    getDomain(){
+        browser.ignoreSynchronization = true;
+        env.setenv(config);
+        var url = 'https://'+ env.getenv();
+        return url;
+        } 
+
+
+
     loginToStudyPlanViaBackDoor(productConfig){
         browser.ignoreSynchronization = true;
         browser.get('https://atom.kaptest.com/login/atom/');
@@ -23,7 +44,7 @@ export class channelPage{
 
     loginToDashboardViaBackDoor(classCode){
         browser.ignoreSynchronization = true;
-        browser.get('https://feature6-atom.kaptest.com/login/atom/');
+        browser.get(this.getDomain()+'/login/atom/');
         browser.sleep(2000);
         element(by.id('DomainUserName')).sendKeys(kec_username);
         element(by.id('DomainPassword')).sendKeys(kec_password);
@@ -31,8 +52,28 @@ export class channelPage{
         element(by.id('ClassCode')).sendKeys(classCode);
         element(by.id('ExternalUserId')).sendKeys('hi');
         element(by.css('.btn-submit')).click(); 
-        browser.get('https://feature6-atom.kaptest.com/dashboard/');
-        browser.sleep(1000);
+        browser.get(this.getDomain()+'/dashboard/');
+        browser.sleep(3000);
+        } 
+
+
+    loginToDashboardViaFrontDoor(){
+        browser.ignoreSynchronization = true;
+        browser.get('http://www.kaptest.com/login.jhtml');
+        browser.sleep(2000);
+        element(by.id('email')).sendKeys('sat6@kaptest.com');
+        element(by.id('password')).sendKeys('kaptest');
+        element(by.xpath('//*[@type="submit"]')).click();
+        browser.sleep(2000);
+        browser.get('https://www.kaptest.com/myaccount');
+        browser.sleep(5000);
+        var dom = this.getDomain();
+        element(by.linkText('Access my Online Resources')).getAttribute('href').then(function(text){
+            browser.get(dom+text.substring(23));
+            browser.sleep(5000);
+        });
+        browser.get(this.getDomain()+'/dashboard/');
+        browser.sleep(5000);
         } 
 
     getDashboardPageTitle(){
@@ -48,6 +89,95 @@ export class channelPage{
             return text;
         });
         }
+
+
+    getContinueLinkAddress(){
+        //browser.ignoreSynchronization = true;
+        return element (by.className('homework--link')).getAttribute('href').then(function(text){
+            return text;
+        });
+        }
+
+    clickContinue(){
+        //browser.ignoreSynchronization = true;
+        element (by.className('homework--link')).click();
+        browser.sleep(5000);
+        }
+
+    
+
+
+    verifyContinueLink(){
+        //browser.ignoreSynchronization = true;
+          return element (by.className('homework--link')).isPresent().then(function(){
+              console.log('Continue link visible');
+              //this.clickContinue();
+              return true;
+          }, function(err){
+                console.error("Continue link not displyed");
+                return false;
+        });
+        }   
+
+
+    verifyLessonTitle(){
+        //browser.ignoreSynchronization = true;
+          return element (by.css('h3')).isPresent().then(function(){
+              console.log('Lesson Title link visible');
+              return true;
+          }, function(err){
+                console.error("Lesson Title not displyed");
+                return false;
+        });
+        } 
+
+    verifyIconPresent(){
+        //browser.ignoreSynchronization = true;
+          return element (by.css(this.ICON)).isPresent().then(function(){
+              console.log('Icon visible');
+              return true;
+          }, function(err){
+                console.error("Icon not displyed");
+                return false;
+        });
+        } 
+
+
+    verifyAssignmentTypePresent(){
+        //browser.ignoreSynchronization = true;
+          return element (by.css(this.AssignmenType)).isPresent().then(function(){
+              console.log('AssignmentType visible');
+              return true;
+          }, function(err){
+                console.error("AssignmentType not displyed");
+                return false;
+        });
+        } 
+
+
+     verifyProgressBarPresent(){
+        //browser.ignoreSynchronization = true;
+          return element (by.css(this.ProgressComplete)).isPresent().then(function(){
+              console.log('% Complete visible');
+              return true;
+          }, function(err){
+                console.error("% Complete not displyed");
+                return false;
+        });
+        } 
+
+    verifyTimeRemainingPresent(){
+        //browser.ignoreSynchronization = true;
+          return element (by.css(this.TimeRemaining)).isPresent().then(function(){
+              console.log('Time visible');
+              return true;
+          }, function(err){
+                console.error("Time not displyed");
+                return false;
+        });
+        } 
+
+
 
 
 }
--- e2e/qbank/channelPageTest-spec.ts
+++ e2e/qbank/channelPageTest-spec.ts
@@ -6,6 +6,7 @@ describe ('Dashboard Tests -', function(){
 
     let channel: channelPage;
 
+
     beforeEach(function () {
         browser.manage().deleteAllCookies();
         channel = new channelPage();
@@ -15,50 +16,132 @@ describe ('Dashboard Tests -', function(){
     //     browser.close();
     // });
 
-    describe('verify channel page ', function(){
+    // describe('verify channel page ', function(){
 
-        var testname= this.getFullName();
+    //     var testname= this.getFullName();
 
 
-        it ('should load channelPage', function(){
-        //browser.ignoreSynchronization = true;
-        console.log('------------'+testname+'------------');
+    //     it ('should load channelPage', function(){
+    //     //browser.ignoreSynchronization = true;
+    //     console.log('------------'+testname+'------------');
         
-        channel.loginToStudyPlanViaBackDoor('lifesupport');
-        expect (browser.getCurrentUrl()).toContain("/studyplan");
-        //browser.sleep(3000);
-        channel.goToChannel();
-        browser.sleep(3000);
-        });
-    });  
+    //     channel.loginToStudyPlanViaBackDoor('lifesupport');
+    //     expect (browser.getCurrentUrl()).toContain("/studyplan");
+    //     //browser.sleep(3000);
+    //     channel.goToChannel();
+    //     browser.sleep(3000);
+    //     });
+    // });  
 
     describe('verifyLastSessionHomeworkComponents', function(){
 
         var testname= this.getFullName();
 
 
-        it ('should load Dashboard', function(){
-        //browser.ignoreSynchronization = true;
-        console.log('------------'+testname+'------------');
-        
-        //Log into Kaptest and go to Dashboard for GRE
-        channel.loginToDashboardViaBackDoor('RAAGL16225');
+        xit ('should load Dashboard', function(){
+            //browser.ignoreSynchronization = true;
+            console.log('------------'+testname+'------------');
+            
+            //Log into Kaptest and go to Dashboard for GRE
+            channel.loginToDashboardViaBackDoor('RAAGL16225');
 
-        //Verify last session homework elements
-        channel.getDashboardPageTitle().then(function(text){
-            expect(text).toBe('Dashboard');
-        });
-        channel.getSessionTitle().then(function(text){
-            expect(text).toBe('After Class');
-        });
+            //Verify last session homework elements
+            channel.getDashboardPageTitle().then(function(text){
+                expect(text).toBe('Dashboard');
+            });
+            browser.sleep(5000);
+            channel.getSessionTitle().then(function(text){
+                expect(text).toBe('After Class');
+            });
+
+            expect (channel.verifyContinueLink()).toBe(true);
+            expect (channel.verifyLessonTitle()).toBe(true);
+            expect (channel.verifyIconPresent()).toBe(true);
+            expect (channel.verifyAssignmentTypePresent()).toBe(true);
+            expect (channel.verifyProgressBarPresent()).toBe(true);
 
 
-
-        browser.sleep(3000);
         });
     });  
 
 
+    describe('verifyContinueLinkWorks', function(){
+
+        var testname= this.getFullName();
+        xit ('should load Dashboard', function(){
+
+            //browser.ignoreSynchronization = true;
+            console.log('------------'+testname+'------------');
+            
+            //Log into Kaptest and go to Dashboard for GRE
+            channel.loginToDashboardViaBackDoor('RAAGL16225');
+
+            //Verify last session homework elements
+            channel.getDashboardPageTitle().then(function(text){
+                expect(text).toBe('Dashboard');
+            });
+
+            browser.sleep(5000);
+            
+            //get href for continue link
+            channel.getContinueLinkAddress().then(function(text){
+                element (by.className('homework--link')).click();
+                browser.sleep(5000);
+                console.log (text);
+                console.log (browser.getCurrentUrl()+'==============');//
+                //expect (browser.getCurrentUrl()).toBe(text);
+            });
+        });  
 
 
+     });    
+
+
+     describe('verify sat account', function(){
+
+        var testname= this.getFullName();
+        xit ('tests front door login + homework as session title', function(){
+            console.log('------------'+testname+'------------');
+            
+            channel.loginToDashboardViaFrontDoor();
+            channel.getSessionTitle().then(function(text){
+                expect(text).toBe('Homework');
+            });
+
+         });  
+     });   
+
+
+
+     describe('verify MCAT account', function(){
+
+    // tests front door login + homework as session title
+        var testname= this.getFullName();
+        xit ('test MCAT account = after class', function(){
+            console.log('------------'+testname+'------------');
+            
+            channel.loginToDashboardViaBackDoor('MRGL14025');
+            channel.getSessionTitle().then(function(text){
+                expect(text).toBe('After Class');
+            });
+
+         });  
+     });
+
+
+    describe('verify no homework', function(){
+
+    // tests front door login + homework as session title
+        var testname= this.getFullName();
+        it ('test MCAT account = after class', function(){
+            console.log('------------'+testname+'------------');
+            
+            channel.loginToDashboardViaBackDoor('LAOTO16033');
+            browser.sleep(5000);
+            element (by.css('.card div p')).getText().then(function(text){
+                expect(text).toBe('NO HOMEWORK YET');
+            });
+
+         });  
+     });
 });
